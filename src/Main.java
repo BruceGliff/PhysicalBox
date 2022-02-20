b8de263293b2eeb4a1af2d0c77e8feff240b1776
Ilya Andreev <ilya.andreev@intel.com>
2022-02-20 12:23:02 -0500

    border init

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ import java.awt.*;
 import java.applet.*;
 
 import geometry.*;
+import objects.*;
 
 public class Main extends Applet
 {
-  Vector p1;
-  int i = 1;
+  Border PlayBorder;
 
   public void init() {
-    Vector p = new Vector(2,3);
-    p1 = new Vector(p);
     setForeground(Color.white);
     setBackground(Color.white);
+
+    PlayBorder = new Border((10,10), (210, 110));
+
   }
   public void paint(Graphics g) {
     g.drawString("Welcome To Java Applet " + Integer.toString(i++) + Float.toString(p1.getY()), 40, 50);
--- src/objects/Border.java
+++ src/objects/Border.java
@@ -1,7 +1,17 @@
 package objects;
 
+import java.awt.*;
+import geometry.*;
+
 public class Border {
-  
+    // Left, up
+    Point LU;
+    // Right, down
+    Point RD;
 
+    public Border() { LU = new Point(0, 0); RD = new Point(100, 200); }
+    public Border(Point LU, Point RD) { this.LU = LU; this.RU = RD; };
 
+    public float getWidth() { return RD.getX() - LU.getX(); }
+    public float getHeight() { return LU.getY() - RD.getY(); }
 }
